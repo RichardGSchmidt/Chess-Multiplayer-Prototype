38d9b18f4aa43b9d4a74ceb6776331de3991022b
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2016-12-30 01:41:12 -0600

    Custom Chess Program From Scratch
    Basic selection functionality implemented, sprite sheet cut, board
    layout decided on.  Next up boundaries and movement!

---
+++ ChessProgram/Chess Program/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GamePiece selected = null;
+    public void SetSelection(GamePiece newSelection)
+    {
+        if (selected != null)
+        {
+            selected.selected = false;  //deselects if no selection availble
+        }
+        selected = newSelection;
+    }
+}
---
+++ ChessProgram/Chess Program/Assets/Scripts/GamePiece.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+    public bool selected = false;
+    public GameManager gameManager;
+    public void Start()
+    {
+        gameManager = GameObject.FindWithTag("Board").GetComponent<GameManager>();
+    }
+    public void OnMouseDown()
+    {
+        selected = true;
+        gameManager.SetSelection(this);
+    }
+
+    public void Deselect()
+    {
+        selected = false;
+    }
+
+}
