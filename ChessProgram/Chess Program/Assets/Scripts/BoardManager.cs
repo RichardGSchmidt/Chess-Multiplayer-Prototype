91171b06e1bf4ccba40ad6c2a8fdc0fd5b4cc6bb
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2016-12-31 08:56:50 -0600

    Board Setup and Modified Sprites
    Board Loader now functions, sprites now more visible on own color.

--- ChessProgram/Chess Program/Assets/Scripts/BoardManager.cs
+++ ChessProgram/Chess Program/Assets/Scripts/BoardManager.cs
@@ -6,25 +6,92 @@ using UnityEngine;
 public class BoardManager : MonoBehaviour
 {
     BoardGrid board;
+    public GamePiece[] gamePieces;
 
 
-    public BoardManager()
+    public void Start()
     {
         board = new BoardGrid();
+        LayOutBoardForPlay();
+
+        
     }
 
-    public BoardManager(BoardGrid boardToBeLoaded)
+    public void LoadBoard(BoardGrid boardToBeLoaded)
     {
         board = boardToBeLoaded;
     }
 
-    public void LayOutBoardForPlay(BoardGrid bord)
+    public void LayOutBoardForPlay()
     {
         for (int x = 0; x < board.spot.GetLength(0); x++)
         {
             for (int y = 0; y < board.spot.GetLength(1); y++)
             {
-
+                if (y == 1)
+                {
+                    GamePiece toInstantiate = gamePieces[9];
+                    board.spot[x,y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                else if(y==6)
+                {
+                    GamePiece toInstantiate = gamePieces[3];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if ((x == 0 && y == 0) ||(x==7&&y==0))
+                {
+                    GamePiece toInstantiate = gamePieces[11];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if ((x == 0 && y == 7) || (x == 7 && y == 7))
+                {
+                    GamePiece toInstantiate = gamePieces[5];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if ((x == 1 && y == 0) || (x == 6 && y == 0))
+                {
+                    GamePiece toInstantiate = gamePieces[8];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if ((x == 1 && y == 7) || (x == 6 && y == 7))
+                {
+                    GamePiece toInstantiate = gamePieces[2];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if ((x == 2 && y == 0) || (x == 5 && y == 0))
+                {
+                    GamePiece toInstantiate = gamePieces[6];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if ((x == 2 && y == 7) || (x == 5 && y == 7))
+                {
+                    GamePiece toInstantiate = gamePieces[0];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if (x == 3 && y == 7)
+                {
+                    GamePiece toInstantiate = gamePieces[4];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if (x == 3 && y == 0)
+                {
+                    GamePiece toInstantiate = gamePieces[10];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if (x==4 && y ==0)
+                {
+                    GamePiece toInstantiate = gamePieces[7];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if (x==4 && y == 7)
+                {
+                    GamePiece toInstantiate = gamePieces[1];
+                    board.spot[x, y].MoveToPosition((Instantiate(toInstantiate, board.spot[x, y].position, Quaternion.identity)));
+                }
+                if(board.spot[x,y].pieceonTile != null)
+                {
+                    board.spot[x, y].pieceonTile.transform.SetParent(GameObject.FindGameObjectWithTag("Board").transform);
+                }
             }
         }
     }
@@ -51,21 +118,21 @@ public class BoardManager : MonoBehaviour
 //Object Container for the entire board and it's layout
 //serialized in order to save it's state to a file.
 
-[System.Serializable]
 public class BoardGrid {
 
     public enum PlayerTurn { White, Black }
     PlayerTurn playerTurn = PlayerTurn.White;
-    public BoardTile[,]spot = new BoardTile[8,8];  //spot will be the actual tile you are refering to.
+    public BoardTile[,] spot = new BoardTile[8, 8];
 
     public BoardGrid()
     {
+        spot = new BoardTile[8,8];
         //filling the transformGrid array
-        for (int x = 0; x < 8; x++)
+        for (int x = 0; x < spot.GetLength(0); x++)
         {
-            for (int y = 0; y < 8; y++)
+            for (int y = 0; y < spot.GetLength(1); y++)
             {
-                spot[x, y].position = new Vector3(-3.5f+x, -3.5f+y, 0);
+                spot[x, y] = new BoardTile(new Vector3(-3.5f+x, -3.5f+y, 0));
             }
         }
     }
@@ -86,8 +153,7 @@ public class BoardGrid {
 }
 
 //object container for an actual tile piece
-//serialized to be able to save this data to file
-[System.Serializable]
+
 public class BoardTile
 {
     public GamePiece pieceonTile;
@@ -97,7 +163,6 @@ public class BoardTile
     private float bottomBoundary;
     private float leftBoundary;
     private float rightBoundary;
-    public BoardTile[,] tilePresets;
 
     //create a new empty board tile
     public BoardTile(Vector3 passed)
