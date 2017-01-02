12ff7c35ca2d18a0a725a0c9941525a0514d569c
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2017-01-02 15:29:13 -0600

    Started new script to replace the Game Manager logic handling turns, needed for eventual networked multiplayer handling.

---
+++ ChessProgram/Chess Program/Assets/Scripts/TurnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//to be used to handle turns in hotseat
+//need pull the logic handling turns out of the raw
+//game manager logic to support networked multiplayer
+public class TurnManager : MonoBehaviour {
+
+    bool turn;
+	// Use this for initialization
+	void Start ()
+    {
+        turn = true;	
+	}
+	
+	// Move mouse handling over to this function instead of on the game manager maybe?
+	void Update ()
+    {
+		
+	}
+}
